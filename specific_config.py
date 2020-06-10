e664ce0f8a38fba1dc56f822d1112916631c797e
IMLHF <red_wind@foxmail.com>
2020-06-10 10:07:49 +0800

    update params

--- specific_config.py
+++ specific_config.py
@@ -1,13 +1,13 @@
 import os
 
-path_to_download = ''
-base_model_path = ''
+path_to_download = 'datasets'
+base_model_path = 'model_saved'
 
 os.makedirs(path_to_download, exist_ok=True)
 os.makedirs(base_model_path, exist_ok=True)
 
 test_model = base_model_path + ''
-cache_dir = ''
+cache_dir = 'cache_unigram'
 
 resume = {
 	'restart': False,
@@ -15,10 +15,10 @@ resume = {
 		''
 }
 
-use_cuda = False
+use_cuda = True
 
 if not use_cuda:
-	num_cuda = '0'
+	num_cuda = '4'
 	os.environ["CUDA_VISIBLE_DEVICES"] = num_cuda
 else:
 	num_cuda = '0'
